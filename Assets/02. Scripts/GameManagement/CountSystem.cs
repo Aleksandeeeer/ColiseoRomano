d8898106cad046651cef281b756ead6935cc55a7
gopo14 <marc1491999@gmail.com>
2023-06-12 20:52:26 +0200

    Script camara + Shaders + animació emperador

--- Assets/02. Scripts/GameManagement/CountSystem.cs
+++ Assets/02. Scripts/GameManagement/CountSystem.cs
@@ -2,12 +2,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Cinemachine;
+using System.Collections;
 
 public class CountSystem : MonoBehaviour
 {
     [SerializeField]
     [Tooltip("The camera that will follow the winner")]
     private CinemachineVirtualCamera _virtualCamera;
+    [SerializeField]
+    [Tooltip("The camera that will follow the cesar")]
+    private CinemachineVirtualCamera _cesarVirtualCamera;
     [SerializeField]
     [Tooltip("The game object that will activate when there is a winner")]
     private GameObject _winnerUIObject;
@@ -17,6 +21,16 @@ public class CountSystem : MonoBehaviour
     [SerializeField]
     [Tooltip("The game object that will always activate to allow the player to exit or replay")]
     private GameObject _replayUI;
+    [SerializeField]
+    private GameObject _cesarObject;
+
+    [Header("Cinematic timings")]
+    [SerializeField]
+    private float _winnerShowTime = 1f;
+    [SerializeField]
+    private float _cesarShowTime = 1f;
+    [SerializeField]
+    private float _loserShowTime = 1f;
 
 
     private List<GameObject> _playerList = new List<GameObject>();
@@ -50,7 +64,7 @@ public class CountSystem : MonoBehaviour
     {
         int max = -1;
         bool tie = true;
-        GameObject current = null;
+        int winner = -1;
         for(int i = 0; i < _playerList.Count; ++i)
         {
             CoinObtainer co = _playerList[i].GetComponent<CoinObtainer>();
@@ -58,25 +72,54 @@ public class CountSystem : MonoBehaviour
             {
                 tie = false;
                 max = co.Coins;
-                current = _playerList[i];
+                winner = i;
             } else if (co.Coins == max)
             {
                 tie = true;
-                current = null;
+                winner = -1;
             }
         }
 
-        if (tie)
+        if (tie || _playerList.Count == 1)
         {
             _tieUIObject.SetActive(true);
+            _replayUI.SetActive(true);
         } else
         {
-            _virtualCamera.Follow = current.transform;
-            _virtualCamera.LookAt = current.transform;
-            _virtualCamera.Priority = 50;
-            _winnerUIObject.SetActive(true);
-            current.GetComponentInChildren<Animator>().SetTrigger("Victory");
+            StartCoroutine(ShowEndgameAnimation(winner));
         }
+    }
+
+    private IEnumerator ShowEndgameAnimation(int winner)
+    {
+        int loser = 1 - winner;
+        var winnerObject = _playerList[winner];
+        var loserObject = _playerList[loser];
+
+        // Follow the winner
+        _virtualCamera.Follow = winnerObject.transform;
+        _virtualCamera.LookAt = winnerObject.transform;
+        _virtualCamera.Priority = 50;
+        _cesarVirtualCamera.Priority = 0;
+        winnerObject.GetComponentInChildren<Animator>().SetTrigger("Victory");
+        yield return new WaitForSeconds(_winnerShowTime);
+
+        // Show the cesar
+        _virtualCamera.Priority = 0;
+        _cesarVirtualCamera.Priority = 50;
+        yield return new WaitForSeconds(0.5f);
+        _cesarObject.GetComponentInChildren<Animator>().SetTrigger("Cesar");
+        yield return new WaitForSeconds(_cesarShowTime);
+
+        // Show the loser
+        _virtualCamera.Follow = loserObject.transform;
+        _virtualCamera.LookAt = loserObject.transform;
+        _cesarVirtualCamera.Priority = 0;
+        _virtualCamera.Priority = 50;
+        loserObject.GetComponentInChildren<Animator>().SetTrigger("Defeat");
+        yield return new WaitForSeconds(_loserShowTime);
+        //_winnerUIObject.SetActive(true);
+
         _replayUI.SetActive(true);
     }
 }
